7259a7191a24b53ce52a750c8306cc89d0d0e4f0
mobin <mobinizadi.dev@gmail.com>
2024-09-17 00:55:07 +0330

    Add the button to add a new note

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,14 +4,14 @@ import { GoSearch } from "react-icons/go";
 export class Header extends Component {
     render() {
         return (
-            <div className='flex justify-between items-center py-5'>
+            <div className='flex justify-between items-center h-28 pr-4'>
                 {/* logo */}
                 <h1 className='text-5xl  font-semibold'>
                     My Note
                 </h1>
                 {/* search */}
-                <div className='flex justify-between items-center rounded-full bg-gray-100 py-3 px-2 basis-1/3'>
-                    <input type="text" placeholder='search ...' className='bg-transparent outline-none h-full' />
+                <div className='flex justify-between items-center rounded-full bg-gray-100 p-3 basis-1/3'>
+                    <input type="text" placeholder='search ...' className='bg-transparent outline-none h-full w-full' />
                     <button>
                         <GoSearch className='w-7 h-7' />
                     </button>
--- src/component/NoteApp/NoteApp.js
+++ src/component/NoteApp/NoteApp.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
 import Header from '../Header'
+import { FaPlusCircle } from "react-icons/fa";
 export class NoteApp extends Component {
     render() {
         return (
             <>
-                <div className='grid grid-cols-12'>
+                <div className='grid grid-cols-12 min-h-screen'>
                     {/* aside */}
-                    <div className='col-span-2'>
+                    <div className='col-span-2 min-h-[calc(100vh-112px)] mt-auto flex flex-col items-center gap-4'>
+                        {/* add new note */}
+                        <div className='flex flex-col items-center'>
+                            <button>
+                                <FaPlusCircle className='w-10 h-10 text-blue-600' />
+                            </button>
+                            <p>Add new</p>
+                        </div>
+                        {/* colors */}
+                        <div className='flex flex-col items-center gap-2'>
+                                <button className='bg-red-500 rounded-full w-6 h-6'>
 
+                                </button>
+
+                        </div>
                     </div>
                     {/* header && main */}
                     <div className='col-span-10'>
-                        <div className="container ">
-                            <Header></Header>
-                        </div>
+                        <Header></Header>
+                        <main className='bg-gray-100 min-h-[calc(100vh-112px)] rounded-t-lg rounded-bl-lg'>
+
+                        </main>
 
                     </div>
 
