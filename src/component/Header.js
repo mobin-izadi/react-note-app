8c0dd3083d813583140b64f36d0e295264081c8a
mobin <mobinizadi.dev@gmail.com>
2024-09-17 12:56:06 +0330

    Add responsive styles

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,20 +4,20 @@ import { GoSearch } from "react-icons/go";
 export class Header extends Component {
     render() {
         return (
-            <div className='flex justify-between items-center h-28 pr-4'>
+            <div className={this.props.className}>
                 {/* logo */}
-                <h1 className='text-5xl  font-semibold'>
+                <h1 className='text-2xl md:text-5xl  font-semibold basis-2/5 md:basis-auto'>
                     My Note
                 </h1>
                 {/* search */}
-                <div className='flex justify-between items-center rounded-full bg-gray-100 p-3 basis-1/3'>
-                    <input type="text" placeholder='search ...' className='bg-transparent outline-none h-full w-full' />
+                <div className='flex justify-between items-center rounded-full bg-gray-100 p-3 basis-full md:basis-1/3 order-3 md:order-none'>
+                    <input type="text" placeholder='search ...' className='bg-transparent outline-none h-full w-full basis-full md:basis-auto' />
                     <button>
                         <GoSearch className='w-7 h-7' />
                     </button>
                 </div>
                 {/* user panel */}
-                <div className='flex items-center gap-1'>
+                <div className='flex items-center justify-end md:justify-normal  gap-1 basis-2/5 md:basis-auto order-2 md:order-none '>
                     <span>Mobin</span>
                     <img src="/images/avatar.jpg" alt="" className='w-10 h-10 rounded-full' />
                 </div>
--- src/component/NoteApp/NoteApp.js
+++ src/component/NoteApp/NoteApp.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import Header from '../Header'
 import { FaPlusCircle } from "react-icons/fa";
+import { FaSquarePlus } from "react-icons/fa6";
+import { MdDelete, MdFavorite, MdEdit } from "react-icons/md";
+
 export class NoteApp extends Component {
     render() {
         return (
             <>
                 <div className='grid grid-cols-12 min-h-screen'>
                     {/* aside */}
-                    <div className='col-span-2 min-h-[calc(100vh-112px)] mt-auto flex flex-col items-center gap-4'>
+                    <div className='col-span-12 md:col-span-2 md:pt-28 flex flex-col items-center gap-4 mb-3 md:mb-0 '>
+                        <Header className='md:hidden flex justify-between gap-2 items-center px-2 my-2 flex-wrap'></Header>
                         {/* add new note */}
                         <div className='flex flex-col items-center'>
                             <button>
@@ -17,17 +21,146 @@ export class NoteApp extends Component {
                         </div>
                         {/* colors */}
                         <div className='flex flex-col items-center gap-2'>
-                                <button className='bg-red-500 rounded-full w-6 h-6'>
+                            <button className='bg-red-500 rounded-full w-6 h-6'>
 
-                                </button>
+                            </button>
 
                         </div>
                     </div>
                     {/* header && main */}
-                    <div className='col-span-10'>
-                        <Header></Header>
-                        <main className='bg-gray-100 min-h-[calc(100vh-112px)] rounded-t-lg rounded-bl-lg'>
+                    <div className='col-span-12 md:col-span-10 mx-4 md:mx-0'>
+                        <Header className='hidden md:flex justify-between items-center h-28 pr-4'></Header>
+                        <main className='bg-gray-100 min-h-[calc(100vh-112px)] rounded-t-lg rounded-bl-lg p-3'>
+                            <div>
+                                <h4 className='font-medium text-2xl'>My favorites</h4>
+                                <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-2 my-3'>
+                                    {/* box */}
+                                    <div style={{ background: '#EFABAC' }} className='col-span-1 rounded-lg p-2 text-black'>
+                                        <div className='flex justify-end'>
+                                            <span>11/2/2024</span>
+                                        </div>
+                                        <p className='text-xl font-medium py-2 border-b '>title</p>
+                                        <p className='py-2 h-36 overflow-auto'>description</p>
 
+                                        <div className='flex justify-around items-center  my-3'>
+                                            <button><MdEdit className='w-8 h-8' /></button>
+                                            <button><MdFavorite className='w-8 h-8 ' /></button>
+                                            <button><MdDelete className='w-8 h-8' /></button>
+                                        </div>
+                                    </div>
+                                    {/* box */}
+                                    <div style={{ background: '#EFABAC' }} className='col-span-1 rounded-lg p-2 text-black'>
+                                        <div className='flex justify-end'>
+                                            <span>11/2/2024</span>
+                                        </div>
+                                        <p className='text-xl font-medium py-2 border-b '>title</p>
+                                        <p className='py-2 h-36 overflow-auto'>description</p>
+
+                                        <div className='flex justify-around items-center  my-3'>
+                                            <button><MdEdit className='w-8 h-8' /></button>
+                                            <button><MdFavorite className='w-8 h-8 ' /></button>
+                                            <button><MdDelete className='w-8 h-8' /></button>
+                                        </div>
+                                    </div>
+                                    {/* box */}
+                                    <div style={{ background: '#EFABAC' }} className='col-span-1 rounded-lg p-2 text-black'>
+                                        <div className='flex justify-end'>
+                                            <span>11/2/2024</span>
+                                        </div>
+                                        <p className='text-xl font-medium py-2 border-b '>title</p>
+                                        <p className='py-2 h-36 overflow-auto'>description</p>
+
+                                        <div className='flex justify-around items-center  my-3'>
+                                            <button><MdEdit className='w-8 h-8' /></button>
+                                            <button><MdFavorite className='w-8 h-8 ' /></button>
+                                            <button><MdDelete className='w-8 h-8' /></button>
+                                        </div>
+                                    </div>
+                                    {/* box */}
+                                    <div style={{ background: '#EFABAC' }} className='col-span-1 rounded-lg p-2 text-black'>
+                                        <div className='flex justify-end'>
+                                            <span>11/2/2024</span>
+                                        </div>
+                                        <p className='text-xl font-medium py-2 border-b '>title</p>
+                                        <p className='py-2 h-36 overflow-auto'>description</p>
+
+                                        <div className='flex justify-around items-center  my-3'>
+                                            <button><MdEdit className='w-8 h-8' /></button>
+                                            <button><MdFavorite className='w-8 h-8 ' /></button>
+                                            <button><MdDelete className='w-8 h-8' /></button>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                            </div>
+
+                            <div>
+                                <h4 className='font-medium text-2xl'>My Note</h4>
+                                <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-2 my-3'>
+                                    {/* box */}
+                                    <div style={{ background: '#EFABAC' }} className='col-span-1 rounded-lg p-2 text-black'>
+                                        <div className='flex justify-end'>
+                                            <span>11/2/2024</span>
+                                        </div>
+                                        <p className='text-xl font-medium py-2 border-b '>title</p>
+                                        <p className='py-2 h-36 overflow-auto'>description</p>
+
+                                        <div className='flex justify-around items-center  my-3'>
+                                            <button><MdEdit className='w-8 h-8' /></button>
+                                            <button><MdFavorite className='w-8 h-8 ' /></button>
+                                            <button><MdDelete className='w-8 h-8' /></button>
+                                        </div>
+                                    </div>
+                                    {/* box */}
+                                    <div style={{ background: '#EFABAC' }} className='col-span-1 rounded-lg p-2 text-black'>
+                                        <div className='flex justify-end'>
+                                            <span>11/2/2024</span>
+                                        </div>
+                                        <p className='text-xl font-medium py-2 border-b '>title</p>
+                                        <p className='py-2 h-36 overflow-auto'>description</p>
+
+                                        <div className='flex justify-around items-center  my-3'>
+                                            <button><MdEdit className='w-8 h-8' /></button>
+                                            <button><MdFavorite className='w-8 h-8 ' /></button>
+                                            <button><MdDelete className='w-8 h-8' /></button>
+                                        </div>
+                                    </div>
+                                    {/* box */}
+                                    <div style={{ background: '#EFABAC' }} className='col-span-1 rounded-lg p-2 text-black'>
+                                        <div className='flex justify-end'>
+                                            <span>11/2/2024</span>
+                                        </div>
+                                        <p className='text-xl font-medium py-2 border-b '>title</p>
+                                        <p className='py-2 h-36 overflow-auto'>description</p>
+
+                                        <div className='flex justify-around items-center  my-3'>
+                                            <button><MdEdit className='w-8 h-8' /></button>
+                                            <button><MdFavorite className='w-8 h-8 ' /></button>
+                                            <button><MdDelete className='w-8 h-8' /></button>
+                                        </div>
+                                    </div>
+                                    {/* box */}
+                                    <div style={{ background: '#EFABAC' }} className='col-span-1 rounded-lg p-2 text-black'>
+                                        <div className='flex justify-end'>
+                                            <span>11/2/2024</span>
+                                        </div>
+                                        <p className='text-xl font-medium py-2 border-b '>title</p>
+                                        <p className='py-2 h-36 overflow-auto'>description</p>
+
+                                        <div className='flex justify-around items-center  my-3'>
+                                            <button><MdEdit className='w-8 h-8' /></button>
+                                            <button><MdFavorite className='w-8 h-8 ' /></button>
+                                            <button><MdDelete className='w-8 h-8' /></button>
+                                        </div>
+                                    </div>
+
+
+
+
+
+                                </div>
+                            </div>
                         </main>
 
                     </div>
